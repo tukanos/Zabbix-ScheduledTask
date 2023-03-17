d3735b29aae96ace8590497e672686cbcc2302f4
Tomilov Alexander <sopholos@sopholos.ru>
2023-03-17 20:35:00 +0500

    rewritten garbage

--- DiscoverScheduledTasks.ps1
+++ DiscoverScheduledTasks.ps1
@@ -5,98 +5,81 @@
 #
 #
 # Add to Zabbix Agent
-#   UserParameter=TaskSchedulerMonitoring[*],powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Program Files\Zabbix Agent\DiscoverScheduledTasks.ps1" "$1" "$2"
+# UserParameter=TaskSchedulerMonitoring[*],powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Program Files\Zabbix Agent\DiscoverScheduledTasks.ps1" "$1" "$2"
 #
 ## Modifier la variable $path pour indiquer les sous dossiers de Tâches Planifiées à traiter sous la forme "\nomDossier\","\nomdossier2\sousdossier\" voir (Get-ScheduledTask -TaskPath )
 ## Change the $path variable to indicate the Scheduled Tasks subfolder to be processed as "\nameFolder\","\nameFolder2\subfolder\" see (Get-ScheduledTask -TaskPath )
+$paths = "\", "\AG\"
 
+Function Convert-ToUnixDate($PSdate) {
+	$epoch = [timezone]::CurrentTimeZone.ToLocalTime([datetime]'1/1/1970')
+	if ($PSdate -eq $Null) {
+		$PSdate = $epoch
+	}
+	(New-TimeSpan -Start $epoch -End $PSdate).TotalSeconds
+}
 
+function Get-ScheduledTaskByFullName($fullname) {
+	$path = $fullname.Substring(0, $fullname.LastIndexOf('\') + 1)
+	$name = $fullname.Substring($fullname.LastIndexOf('\') + 1)
 
-$path = "\"
+	return Get-ScheduledTask -TaskPath "$path" -TaskName "$name"
+}
 
+function Get-ScheduledTaskInfoByFullName($fullname) {
+	$path = $fullname.Substring(0, $fullname.LastIndexOf('\') + 1)
+	$name = $fullname.Substring($fullname.LastIndexOf('\') + 1)
 
-
-
-Function Convert-ToUnixDate ($PSdate) {
-   $epoch = [timezone]::CurrentTimeZone.ToLocalTime([datetime]'1/1/1970')
-   if ($PSdate -eq $Null) {
-       $PSdate = $epoch
-   }
-   (New-TimeSpan -Start $epoch -End $PSdate).TotalSeconds
+	return Get-ScheduledTaskInfo -TaskPath "$path" -TaskName "$name"
 }
 
 $ITEM = [string]$args[0]
 $ID = [string]$args[1]
 
 switch ($ITEM) {
-  "DiscoverTasks" {
-$apptasks = Get-ScheduledTask -TaskPath $path | where {$_.state -like "Ready" -and "Running"}
-$apptasksok1 = $apptasks.TaskName
-$apptasksok = $apptasksok1.replace('â','&acirc;').replace('à','&agrave;').replace('ç','&ccedil;').replace('é','&eacute;').replace('è','&egrave;').replace('ê','&ecirc;')
-$idx = 1
-write-host "{"
-write-host " `"data`":[`n"
-foreach ($currentapptasks in $apptasksok)
-{
-    if ($idx -lt $apptasksok.count)
-    {
-     
-        $line= "{ `"{#APPTASKS}`" : `"" + $currentapptasks + "`" },"
-        write-host $line
-    }
-    elseif ($idx -ge $apptasksok.count)
-    {
-    $line= "{ `"{#APPTASKS}`" : `"" + $currentapptasks + "`" }"
-    write-host $line
-    }
-    $idx++;
-} 
-write-host
-write-host " ]"
-write-host "}"}}
+	"DiscoverTasks" {
+		$data = @()
+		foreach ($path in $paths) {
+			$apptasks = Get-ScheduledTask -TaskPath $path -ErrorAction Ignore | where {$_.state -like "Ready" -and "Running"}
+			if ($NULL -eq $apptasks) { continue }
 
+			foreach ($currentapptasks in $apptasks)	{
+				# $apptasksok = $apptasksok1.replace('â','&acirc;').replace('à','&agrave;').replace('ç','&ccedil;').replace('é','&eacute;').replace('è','&egrave;').replace('ê','&ecirc;')
+			
+				$data += @{ "{#APPTASKS}" = $currentapptasks.TaskPath + $currentapptasks.TaskName }				
+			}
+		}
 
-switch ($ITEM) {
-  "TaskLastResult" {
-[string] $name = $ID
-$name1 = $name.replace('&acirc;','â').replace('&agrave;','à').replace('&ccedil;','ç').replace('&eacute;','é').replace('&egrave;','è').replace('&ecirc;','ê')
-$pathtask = Get-ScheduledTask -TaskPath "*" -TaskName "$name1"
-$pathtask1 = $pathtask.Taskpath
-$taskResult = Get-ScheduledTaskInfo -TaskPath "$pathtask1" -TaskName "$name1"
-Write-Output ($taskResult.LastTaskResult)
-}}
+		$json = @{ "data" = $data }
+		$json | ConvertTo-Json | Write-Host
+	}
 
-switch ($ITEM) {
-  "TaskLastRunTime" {
-[string] $name = $ID
-$name1 = $name.replace('&acirc;','â').replace('&agrave;','à').replace('&ccedil;','ç').replace('&eacute;','é').replace('&egrave;','è').replace('&ecirc;','ê')
-$pathtask = Get-ScheduledTask -TaskPath "*" -TaskName "$name1"
-$pathtask1 = $pathtask.Taskpath
-$taskResult = Get-ScheduledTaskInfo -TaskPath "$pathtask1" -TaskName "$name1"
-$taskResult1 = $taskResult.LastRunTime
-$date = get-date -date "01/01/1970"
-$taskResult2 = Convert-ToUnixDate($taskResult1)
-Write-Output ($taskResult2)
-}}
+	"TaskLastResult" {
+		$taskResult = Get-ScheduledTaskInfoByFullName $ID
+		Write-Output ($taskResult.LastTaskResult)
+	}
 
-switch ($ITEM) {
-  "TaskNextRunTime" {
-[string] $name = $ID
-$name1 = $name.replace('&acirc;','â').replace('&agrave;','à').replace('&ccedil;','ç').replace('&eacute;','é').replace('&egrave;','è').replace('&ecirc;','ê')
-$pathtask = Get-ScheduledTask -TaskPath "*" -TaskName "$name1"
-$pathtask1 = $pathtask.Taskpath
-$taskResult = Get-ScheduledTaskInfo -TaskPath "$pathtask1" -TaskName "$name1"
-$taskResult1 = $taskResult.NextRunTime
-$date = get-date -date "01/01/1970"
-$taskResult2 = Convert-ToUnixDate($taskResult1)
-Write-Output ($taskResult2)
-}}
+	"TaskLastRunTime" {
+		$taskResult = Get-ScheduledTaskInfoByFullName $ID
 
-switch ($ITEM) {
-  "TaskState" {
-[string] $name = $ID
-$name1 = $name.replace('&acirc;','â').replace('&agrave;','à').replace('&ccedil;','ç').replace('&eacute;','é').replace('&egrave;','è').replace('&ecirc;','ê')
-$pathtask = Get-ScheduledTask -TaskPath "*" -TaskName "$name1"
-$pathtask1 = $pathtask.State
-Write-Output ($pathtask1)
-}}
+		$taskResult1 = $taskResult.LastRunTime
+		$date = get-date -date "01/01/1970"
+		$taskResult2 = Convert-ToUnixDate($taskResult1)
+		Write-Output ($taskResult2)
+	}
+
+	"TaskNextRunTime" {
+		$taskResult = Get-ScheduledTaskInfoByFullName $ID
+
+		$taskResult1 = $taskResult.NextRunTime
+		$date = get-date -date "01/01/1970"
+		$taskResult2 = Convert-ToUnixDate($taskResult1)
+		Write-Output ($taskResult2)
+	}
+
+	"TaskState" {
+		$pathtask = Get-ScheduledTaskByFullName $ID
+		$pathtask1 = $pathtask.State
+		Write-Output ($pathtask1)
+	}
+}
