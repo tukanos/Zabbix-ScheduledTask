0ee89b306116e5b1d16ff225899469d3f8cd4c4c
Patrik Svestka <patrik.svestka@gmail.com>
2024-04-11 09:34:18 +0200

    Adding missing parameter for Zabbix UserParameter in the comment

--- DiscoverScheduledTasks.ps1
+++ DiscoverScheduledTasks.ps1
@@ -1,11 +1,13 @@
 # Script: DiscoverSchelduledTasks
 # Author: Romain Si
 # Revision: Isaac de Moraes
+# Revision: Patrik Svestka
+#
 # This script is intended for use with Zabbix > 3.x
 #
 #
 # Add to Zabbix Agent
-# UserParameter=TaskSchedulerMonitoring[*],powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Program Files\Zabbix Agent\DiscoverScheduledTasks.ps1" "$1" "$2"
+# UserParameter=TaskSchedulerMonitoring[*],powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Program Files\Zabbix Agent\DiscoverScheduledTasks.ps1" "$1" "$2" "$3"
 #
 ## Modifier la variable $path pour indiquer les sous dossiers de Tâches Planifiées à traiter sous la forme "\nomDossier\","\nomdossier2\sousdossier\" voir (Get-ScheduledTask -TaskPath )
 ## Change the $path variable to indicate the Scheduled Tasks subfolder to be processed as "\nameFolder\","\nameFolder2\subfolder\" see (Get-ScheduledTask -TaskPath )
