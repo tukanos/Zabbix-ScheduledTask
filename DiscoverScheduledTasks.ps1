66a1ead0cc4743b73c511938abfeacc4e2f1f7c8
Patrik Svestka <patrik.svestka@gmail.com>
2024-04-19 13:05:22 +0200

    Accented characters are replaced by the non-accented equivalents   In powershell split APPTASKS:     - APPTASKSNAME - task name which will be displayed in Zabbix     - APPTASKKEY` - task name which will be used for getting values   Adjusted the XML template for zabbix 6.0 and created new for 6.4

--- DiscoverScheduledTasks.ps1
+++ DiscoverScheduledTasks.ps1
@@ -1,13 +1,8 @@
 # Script: DiscoverSchelduledTasks
 # Author: Romain Si
 # Revision: Isaac de Moraes
-# Revision: Patrik Svestka
-#
-# This script is intended for use with Zabbix > 3.x
-# Note: This script uses front slash in the path instead of Task Schedule's backslash path (XML) as the backslash are considered unsafe character in Zabbix
-#
-# Add to Zabbix Agent
-# UserParameter=TaskSchedulerMonitoring[*],powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Program Files\Zabbix Agent\DiscoverScheduledTasks.ps1" "$1" "$2" "$3"
+# update: Patrik Svestka
+# This script is intended for use with Zabbix > 6.x
 #
 # Note: should there be a need to use wild char star (*) in the task path - it is considered to be an unsafe character in Zabbix- it needs to be enabled
 #       in zabbix_agent2.conf using UnsafeUserParameters=1
@@ -18,13 +13,26 @@
 #   `.\DiscoverSchelduledTasks.ps1 '/Accounting/*,/Backup/' 'DiscoverTasks'`
 # 2) Show the last task result for the AccoungDailyBackup task
 #    `'.\DiscoverSchelduledTasks.ps1 '/Accounting/' 'TaskLastResult' 'AccountingDailyBackup'`
+#
+# Add to Zabbix Agent
+# UserParameter=TaskSchedulerMonitoring[*],powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Program Files\Zabbix Agent\DiscoverScheduledTasks.ps1" "$1" "$2" "$3"
+#
+## Modifier la variable $path pour indiquer les sous dossiers de Tâches Planifiées à traiter sous la forme "\nomDossier\","\nomdossier2\sousdossier\" voir (Get-ScheduledTask -TaskPath )
+## Change the $path variable to indicate the Scheduled Tasks subfolder to be processed as "\nameFolder\","\nameFolder2\subfolder\" see (Get-ScheduledTask -TaskPath )
 
 $ErrorActionPreference = "Stop"
 
 function Convert-ToUnixDate($taskTime) {
     $epoch = [timezone]::CurrentTimeZone.ToLocalTime([datetime]'1/1/1970')
     if ($taskTime -eq $Null) { $taskTime = $epoch }
+    
     return (New-TimeSpan -Start $epoch -End $taskTime).TotalSeconds
+}
+
+function ReplaceAccentedCharacters {
+    param ([string]$StringToBeConverted)
+    
+    return [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($StringToBeConverted))
 }
 
 # Note: Not using Get-ScheduledTaskInfo directly as it can not deal correctly with accented or wild characters
@@ -37,9 +45,9 @@ function Get-ScheduledTaskByFullName($path, $name) {
 }
 
 # Zabbix template is using the forward slash (/) instead of the Task schedule XML's backslash (\) - changing to the proper XML's style
+# Note: The backslash and star (*) are considered unsafe character in Zabbix
 $taskPath = ([string]$args[0]).replace('/','\')
 $taskAction = [string]$args[1]
-# getting only portion task name from the task path and name
 $taskName = ([string]$args[2]).Substring(([string]$args[2]).LastIndexOf('/') + 1)
 
 switch ($taskAction) {
@@ -50,7 +58,10 @@ switch ($taskAction) {
             $appTasks = Get-ScheduledTask -TaskPath $path | where { $_.state -ne "Disabled" }
             if ($appTasks -eq $null) { continue }
             foreach ($currentAppTasks in $appTasks) {
-                $data += @{ '{#APPTASKS}' = $currentAppTasks.TaskPath.replace('\','/') + $currentAppTasks.TaskName }
+                $data += @{
+                    '{#APPTASKSNAME}' = $currentAppTasks.TaskPath.replace('\','/') + (ReplaceAccentedCharacters ($currentAppTasks.TaskName))
+                    '{#APPTASKSKEY}' = $currentAppTasks.TaskPath.replace('\','/') + $currentAppTasks.TaskName
+                }
             }
         }
         $json = @{ 'data' = $data }
@@ -78,8 +89,9 @@ switch ($taskAction) {
         $taskState = (Get-ScheduledTaskByFullName $taskPath $taskName).State
         Write-Output $taskState
     }
-
+    
     Default {
-        throw "Error trying getting  task action: $taskAction"
+        throw "Error trying getting task action: $taskAction"
+        exit 1
     }
 }
